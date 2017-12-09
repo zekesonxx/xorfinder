35a3125307a445b951be4271b0815cd83f05cbed
Zeke Sonxx <zeke@zekesonxx.com>
2017-12-09 14:12:26 -0500

    Add --visual and --xored flags

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ fn main() {
         (about: "Given a needle and a haystack, find if the needle occurs XORed anywhere in the stack")
         (@arg needle: +required "Needle to find (hex bytes or filename)")
         (@arg haystack: +required "Haystack to find the needle in (hex bytes or filename)")
+        (@arg visual: -v --visual "Display the position visually")
+        (@arg xored: -x --xored "Display the needle value in it's found XORed form")
     ).get_matches();
     let needle = matches.value_of("needle").unwrap();
     let needle: Vec<u8> = match FromHex::from_hex(needle.as_bytes().to_owned()) {
@@ -42,10 +44,14 @@ fn main() {
     };
 
     let mut found = false;
+    let mut offset = 0;
+    let mut xor = 0;
     for i in 0u8..255 {
         match test_xor(needle.as_ref(), haystack.as_ref(), i) {
             Some(k) => {
                 println!("Found needle in haystack at offset {} XOR {}", k, i);
+                offset = k;
+                xor = i;
                 found = true;
                 break;
             },
@@ -54,5 +60,27 @@ fn main() {
     }
     if !found {
         println!("Needle not found in haystack with any XOR value");
+    } else if matches.is_present("visual") {
+        for i in 0..haystack.len() {
+            if i == offset {
+                print!("\x1B[1m");
+            }
+            print!("{:02X}", haystack[i]);
+            if i == offset+needle.len()-1 {
+                print!("\x1B[0m");
+            }
+        }
+        println!();
+    }
+
+    if found && matches.is_present("xored") {
+        for &byte in needle.iter() {
+            print!("{:02X}", byte);
+        }
+        print!(" XOR {} = ", xor);
+        for &byte in needle.iter() {
+            print!("{:02X}", byte^xor);
+        }
+        println!();
     }
 }
